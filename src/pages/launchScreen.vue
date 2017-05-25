3439cf3886dce20ae27a96bcbab866a96126681c
胡卓萌 <285261254@qq.com>
2017-05-25 15:24:53 +0800

    image show at frontend

--- src/pages/launchScreen.vue
+++ src/pages/launchScreen.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <image class="img" resize="cover" src='../resource/image/login/launchScreen'></image>
+        <image class="img" resize="cover" src='../res/image/login/launch.png'></image>
+        <!-- <text @click="ttt">{{aaa}}</text> -->
     </div>
 </template>
 
@@ -10,3 +11,25 @@
         height: 1334px;
     }
 </style>
+
+<script>
+    export default {
+        data () {
+            return {
+                aaa : "xxxxx"
+            }
+        },
+
+        created : function() {
+            this.aaa = "sadsdsd"
+            console.log(this.aaa)
+        },
+
+        methods : {
+            ttt : function() {
+                this.aaa = "111"
+                console.log(this)
+            }
+        }
+    }
+</script>
